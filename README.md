f544ae6072331378b6d9423c55a54af2d54e7d16
Juraj Husar <jurosh@gmail.com>
2017-09-30 10:39:48 +0200

    Added readme, updated and cleaned code

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple GraphQL server
+
+Using express.js
+
+## Setup & Run
+
+```
+npm install
+npm start
+```
+
+Open `https://localhost/graphql`
--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const PORT = 443;
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
-var root = {
+const root = {
   hello: () => {
     return 'Hello world!';
   },
 };
 
-var app = express();
+const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
 
-app.listen(443);
+app.listen(PORT);
 
-console.log('Running a GraphQL API server at localhost:443/graphql');
+console.log(`Running a GraphQL API server at localhost:${PORT}/graphql`);
